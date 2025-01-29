365eb6af1ca1bce5b8fcfa95f5e142a5d706d3df
Yugoge <yugetang@outlook.com>
2025-01-29 20:00:30 +0800

    Update book.py

--- weread2notionpro/book.py
+++ weread2notionpro/book.py
@@ -71,10 +71,10 @@ def insert_book_to_notion(books, index, book_id):
                 for x in book.get("categories")
             ]
     properties = utils.get_properties(book, book_properties_type_dict)
-    if book.get("时间"):
+    if book.get("Date"):
         notion_helper.get_date_relation(
             properties,
-            pendulum.from_timestamp(book.get("时间"), tz="Asia/Shanghai"),
+            pendulum.from_timestamp(book.get("Date"), tz="Asia/Shanghai"),
         )
 
     print(
